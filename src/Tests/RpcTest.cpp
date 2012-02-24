f1f6dad09c377c6aa558d5fb68e1c471a7832490
David Isaac Wolinsky <isaac.wolinsky@gmail.com>
2012-02-24 15:00:20 -0500

    Goes with previous commit

--- src/Tests/RpcTest.cpp
+++ src/Tests/RpcTest.cpp
@@ -4,12 +4,12 @@ namespace Dissent {
 namespace Tests {
   TEST(Rpc, HelloWorld)
   {
-    QSharedPointer<RpcHandler> rpc0(new RpcHandler());
+    RpcHandler rpc0;
     QSharedPointer<MockSource> ms0(new MockSource());;
     ms0->SetSink(rpc0);
     QSharedPointer<MockSender> to_ms0(new MockSender(ms0));
 
-    QSharedPointer<RpcHandler> rpc1(new RpcHandler());
+    RpcHandler rpc1;
     QSharedPointer<MockSource> ms1(new MockSource());;
     ms1->SetSink(rpc1);
     QSharedPointer<MockSender> to_ms1(new MockSender(ms1));
@@ -20,7 +20,7 @@ namespace Tests {
     QSharedPointer<RequestHandler> req_h(new RequestHandler());
     QObject::connect(req_h.data(), SIGNAL(MakeRequestSignal(const Request &)),
         &test0, SLOT(Add(const Request &)));
-    rpc0->Register("add", req_h);
+    rpc0.Register("add", req_h);
 
     QVariantList data;
     data.append(3);
@@ -33,27 +33,27 @@ namespace Tests {
         &test1, SLOT(HandleResponse(const Response &)));
 
     EXPECT_EQ(0, test1.GetValue());
-    rpc1->SendRequest(to_ms0, "add", data, res_h);
+    rpc1.SendRequest(to_ms0, "add", data, res_h);
     EXPECT_EQ(9, test1.GetValue());
     EXPECT_TRUE(test1.GetResponse().Successful());
 
     data[1] = "Haha";
-    rpc1->SendRequest(to_ms0, "add", data, res_h);
+    rpc1.SendRequest(to_ms0, "add", data, res_h);
     EXPECT_EQ(0, test1.GetValue());
     EXPECT_FALSE(test1.GetResponse().Successful());
 
     data[0] = "Haha";
-    rpc1->SendRequest(to_ms0, "add", data, res_h);
+    rpc1.SendRequest(to_ms0, "add", data, res_h);
     EXPECT_EQ(0, test1.GetValue());
     EXPECT_FALSE(test1.GetResponse().Successful());
 
     data[0] = 8;
     data[1] = 2;
-    rpc1->SendRequest(to_ms0, "add", data, res_h);
+    rpc1.SendRequest(to_ms0, "add", data, res_h);
     EXPECT_EQ(10, test1.GetValue());
     EXPECT_TRUE(test1.GetResponse().Successful());
 
-    rpc1->SendRequest(to_ms0, "Haha", data, res_h);
+    rpc1.SendRequest(to_ms0, "Haha", data, res_h);
     EXPECT_EQ(0, test1.GetValue());
     EXPECT_FALSE(test1.GetResponse().Successful());
   }
